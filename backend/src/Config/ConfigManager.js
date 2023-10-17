23d98aea4441a08956a46b9023ba1028e5e31138
Alex Delaveau <alex26delaveau@gmail.com>
2023-10-17 15:17:27 +0200

    connection to db with no username/password

--- backend/src/Config/ConfigManager.js
+++ backend/src/Config/ConfigManager.js
@@ -9,9 +9,21 @@ export default class ConfigManager {
     this.api = {
         port: process.env.API || 3000
     }
+
+    this.db = {
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 3306,
+        database: process.env.DB_NAME || "database",
+        username: process.env.DB_USERNAME || "root",
+        password: process.env.DB_PASSWORD || "",
+    }
   }
 
   get apiPort(){
     return this.api.port;
+  }
+
+  get dbConfig(){
+    return this.db;
   }
 }
