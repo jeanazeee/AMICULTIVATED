360f9edca8ee1d477e60ac73b5c6de2106de33a0
jeanazeee <nat.ernest@live.fr>
2023-10-18 09:53:14 +0200

    Fin routage pages

--- amicultivated_frontend/src/router/index.js
+++ amicultivated_frontend/src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Jeu from '../views/game/Jeu.vue'
+import FinDeJeu from '../views/game/FinDeJeu.vue'
+import CreationPartie from '../views/game/CreationPartie.vue'
+import OeuvreJour from '../views/game/OeuvreJour.vue'
+import Connexion from '../views/user/Connexion.vue'
+import Profil from '../views/user/Profil.vue'
+import Stats from '../views/user/Stats.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +15,41 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/creer',
+      name: 'creer',
+      component: Home
+    },
+    {
+      path: '/fin',
+      name: 'findejeu',
+      component: FinDeJeu
+    },
+    {
+      path: '/partie',
+      name: 'partie',
+      component: Jeu
+    },
+    {
+      path: '/quotidient',
+      name: 'quotidient',
+      component: OeuvreJour
+    },
+    {
+      path: '/connexion',
+      name: 'connexion',
+      component: Connexion
+    },
+    {
+      path: '/profil',
+      name: 'profil',
+      component: Profil
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: Stats
     }
   ]
 })
