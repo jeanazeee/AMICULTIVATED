2a1fbd004bd0a7f9bb2482fd5ad984847d386456
Alex Delaveau <alex26delaveau@gmail.com>
2023-11-16 10:56:42 +0100

    frontend for joining/leaving/creating room

--- amicultivated_frontend/src/components/Room.vue
+++ amicultivated_frontend/src/components/Room.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="greetings">
-        <button @click="joinRoom()">Join Room</button>
-        <button @click="leaveRoom()">Leave Room</button>
-        <input type="text" name="" id="" v-model="roomCode" >
+        <div class="create">
+            <button @click="createRoom()">Create Room</button>
+            <p id="room-code"></p>
+        </div>
+        <div class="join-leave">
+            <button @click="joinRoom()">Join Room</button>
+            <button @click="leaveRoom()">Leave Room</button>
+            <input type="text" name="" id="" v-model="roomCode" >
+        </div>
     </div>
   </template>
 
@@ -23,5 +29,44 @@ const leaveRoom = () => {
     api.leaveRoom(roomCode.value, "")
 }
 
-
+const createRoom = async () => {
+    try {
+        let roomCode = await api.createRoom();
+        console.log(roomCode);
+        document.getElementById("room-code").innerHTML = "Room code : " + roomCode;
+    } catch (error) {
+        console.error('Error in createRoom:', error);
+    }
+}
 </script>
+
+<style scoped>
+
+/* greetings take all the page, and each div inside take 50 width */
+
+.greetings {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+}
+
+.create {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+}
+
+.join-leave {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+}
+</style>
+
