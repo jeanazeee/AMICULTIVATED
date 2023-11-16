73cb28a70763a29a55abe8ab788cfe1976d611a6
Alex Delaveau <alex26delaveau@gmail.com>
2023-11-16 18:52:26 +0100

    Added page to login and sign up. connected to backend but not to vuex store

--- amicultivated_frontend/src/router/index.js
+++ amicultivated_frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/user/Login.vue'
+import Signup from '../views/user/Signup.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name:'login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name:'signup',
+      component: Signup
     }
   ]
 })
