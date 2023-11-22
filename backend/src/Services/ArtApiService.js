6e5b51fb89c11b36f6751e6cf99338caf994d065
Alex Delaveau <alex26delaveau@gmail.com>
2023-11-22 16:53:44 +0100

    Linked Service and API + all art between page endpoit

--- backend/src/Services/ArtApiService.js
+++ backend/src/Services/ArtApiService.js
@@ -1,8 +1,40 @@
+import axios from 'axios';
+
 class ArtApiService {
+    static url = "http://www.wikiart.org/en/api/2/MostViewedPaintings";
 
+    static async getArt(pageStart, pageEnd) {
+        let artData;
+        let currentPaginationToken = '';
 
-    static getAllArt() {
-        return fetch('http://localhost:3000/art')
-            .then(response => response.json())
+        for (let page = 1; page <= pageEnd; page++) {
+            try {
+                const urlWithToken = `${ArtApiService.url}?paginationToken=${currentPaginationToken}`;
+                const response = await axios.get(urlWithToken);
+
+                if (response.status === 200) {
+                    const jsonData = response.data;
+                    currentPaginationToken = jsonData.paginationToken;
+                    if (page >= pageStart && page <= pageEnd) {
+                        if (!artData) {
+                            artData = jsonData; // Assign initial data
+                        } else {
+                            // Check if artData has 'data' property before using concat
+                            if (artData.data && Array.isArray(artData.data) && jsonData.data && Array.isArray(jsonData.data)) {
+                                artData.data = artData.data.concat(jsonData.data);
+                            } else {
+                                console.error('Invalid data structure in response data.');
+                            }
+                        }
+                    }
+                } else {
+                    console.error('Request failed with status:', response.status);
+                }
+            } catch (error) {
+                console.error('Error fetching art:', error);
+            }
+        }
+        return artData;
     }
 }
+export default ArtApiService;
